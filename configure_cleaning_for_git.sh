5575e28ae030ce345ee719235250429f9824f1c9
Aldana <aldanuchi@gmail.com>
2025-04-04 22:11:45 -0300

    ops: actualizo filtro para no committear las imgs en outputs de los commits

--- configure_cleaning_for_git.sh
+++ configure_cleaning_for_git.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Este script configura el filtro definido en .gitattributes (escribe en el .git/config local de este repositorio)
-# Configura git para eliminar la metadata generada en las celdas de los notebooks para no committearlas
+# Configura git para excluir la metadata generada en las celdas de los notebooks y las imágenes
+# (que se leen como tiras de bytes) de git, para no committearlas
 git config --local filter.clean_ipynb.clean "jq --indent 1 '(.cells[] | select(has(\"execution_count\")) | .execution_count) = null  \
 | .metadata = {\"language_info\": {\"name\": \"python\", \"pygments_lexer\": \"ipython3\"}} \
-| .cells[].metadata = {}'"
+| .cells[].metadata = {} \
+| (.cells[].outputs[]? | select(.data? and .data[\"image/png\"])) |= del(.data[\"image/png\"])'"
 git config --local filter.clean_ipynb.smudge "cat"
 git config --local filter.clean_ipynb.required true
