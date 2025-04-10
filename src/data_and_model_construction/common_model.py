6f422864eb1fa1dcb03dd153f7ba08e548d9a823
Aldana <aldanuchi@gmail.com>
2025-04-09 22:31:19 -0300

    (minor rename)

--- src/data_and_model_construction/common_model.py
+++ src/data_and_model_construction/common_model.py
@@ -14,37 +14,36 @@ def create_model(data_dict):
     mdl = Model(name)
     
     prod_names = [prod[0] for prod in products]
-    produccion_vars = mdl.continuous_var_dict(prod_names, name=prod_names)#, key_format="%s")
+    production_vars = mdl.continuous_var_dict(prod_names, name=prod_names)#, key_format="%s")
 
     # Debug
     # print(f"products: {products}")
     # print(f"prod_names: {prod_names}")
-    # print(f"produccion_vars: {produccion_vars}")
+    # print(f"production_vars: {production_vars}")
     # print(f"resources: {resources}")
     # print(f"consumptions: {consumptions}")
     # print(f"")
-    # print(f"produccion_vars['A']: {produccion_vars['A']}")
+    # print(f"production_vars['A']: {production_vars['A']}")
     # --- constraints ---
 
     # resources disp equipo and consumptions
-    mdl.add_constraints((mdl.sum(produccion_vars[p[0]] * consumptions[res[0]][products.index(p)] for p in products) <= res[1], 'Disp_%s' % res[0]) for res in resources)
+    mdl.add_constraints((mdl.sum(production_vars[p[0]] * consumptions[res[0]][products.index(p)] for p in products) <= res[1], 'Disp_%s' % res[0]) for res in resources)
 
     # max demand
-    #mdl.add_constraints((produccion_vars[p] <= p[2], 'DemandMax_%s' % p[0]) for p in products)
-    mdl.add_constraints((produccion_vars[p[0]] <= p[2], 'DemandMax_%s' % p[0]) for p in products)
+    mdl.add_constraints((production_vars[p[0]] <= p[2], 'DemandMax_%s' % p[0]) for p in products)
 
     # min demand
-    mdl.add_constraints((produccion_vars[p[0]] >= p[3], 'DemandMin_%s' % p[0]) for p in products)
+    mdl.add_constraints((production_vars[p[0]] >= p[3], 'DemandMin_%s' % p[0]) for p in products)
 
     # --- print information ---
     mdl.print_information()
 
-    total_benefit = mdl.sum(produccion_vars[p[0]] * p[1] for p in products)
+    total_benefit = mdl.sum(production_vars[p[0]] * p[1] for p in products)
 
     # --- set the objective ---
     mdl.maximize(total_benefit)
 
-    return mdl, produccion_vars, products
+    return mdl, production_vars, products
 
 # Print model human friendly name, restrictions and objective.
 # Do not print restrictions such as ">=0" nor "<= inf".
@@ -78,7 +77,7 @@ def print_model(mdl):
     print("--------------------")
 
 # Solve the model
-def solve_model(mdl, produccion_vars, products):
+def solve_model(mdl, production_vars, products):
     solution = mdl.solve()
 
     if not solution:
@@ -90,4 +89,4 @@ def solve_model(mdl, produccion_vars, products):
     print("* Production model solved with objective: {:g}".format(obj))
     print("* Total benefit=%g" % mdl.objective_value)
     for p in products:
-        print("Production of {product}: {prod_var}".format(product=p[0], prod_var=produccion_vars[p[0]].solution_value))
+        print("Production of {product}: {prod_var}".format(product=p[0], prod_var=production_vars[p[0]].solution_value))
